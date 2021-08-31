8685b61287b812b6c507b0b7e99197e997615eb5
yxj0312 <yanxiajun@hotmail.com>
2021-08-31 23:12:15 +0200

    add css module

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-// import './style.css'
+import './style.css'
+import styles from './example.module.css'
 
-// document.querySelector('#app').innerHTML = `
-//   <h1>Hello Vite!</h1>
-//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-// `
+document.querySelector('#app').innerHTML = `
+  <h1 class="${styles.title}">Hello Vite!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`
