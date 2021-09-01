d8f56655bd1af3fa71c234a8315abcb39a61d1f4
yxj0312 <yanxiajun@hotmail.com>
2021-09-01 22:52:42 +0200

    change the html back to stripe dropdown

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-import './style.css'
-import styles from './example.module.css'
-import favicon from './favicon.svg'
+// import './style.css'
+// import styles from './example.module.css'
+// import favicon from './favicon.svg'
 
-console.log(favicon)
+// console.log(favicon)
 
-document.querySelector('#app').innerHTML = `
-  <h1 class="${styles.title}">Hello Vite!</h1>
-  <img src="${favicon}">
-  <img src="./favicon.svg">
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+// document.querySelector('#app').innerHTML = `
+//   <h1 class="${styles.title}">Hello Vite!</h1>
+//   <img src="${favicon}">
+//   <img src="./favicon.svg">
+//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+// `
