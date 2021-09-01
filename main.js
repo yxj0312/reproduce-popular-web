eeea2432709bee2f0b8c557c6c0c3a9658f82431
yxj0312 <yanxiajun@hotmail.com>
2021-09-01 22:23:11 +0200

    add asset

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 import './style.css'
 import styles from './example.module.css'
+import favicon from './favicon.svg'
+
+console.log(favicon)
 
 document.querySelector('#app').innerHTML = `
   <h1 class="${styles.title}">Hello Vite!</h1>
+  <img src="${favicon}">
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
