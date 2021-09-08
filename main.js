a18a733a447b82e4c3334ff02532c517ca51f647
yxj0312 <yanxiajun@hotmail.com>
2021-09-08 22:05:19 +0200

    add arrow transition

--- main.js
+++ main.js
@@ -13,6 +13,8 @@
 
 const menuItems = document.querySelectorAll('nav ul li')
 const menuBackground = document.querySelector('.menu-background')
+const nav = document.querySelector('nav ul')
+const arrow = document.querySelector('.arrow')
 
 menuItems.forEach(menuItem => menuItem.addEventListener('mouseenter', handleEnter))
 menuItems.forEach(menuItem => menuItem.addEventListener('mouseleave', handleLeave))
@@ -25,6 +27,7 @@ function handleEnter() {
     setTimeout(() => menu.classList.add('menu-enter-active'), 50);
 
     const menuCoords = menu.getBoundingClientRect()
+    const navCoords = nav.getBoundingClientRect()
 
     // menuBackground.style.setProperty('width',`${menuCoords.width}px`)
     // menuBackground.style.setProperty('height',`${menuCoords.height}px`)
@@ -37,9 +40,13 @@ function handleEnter() {
       scaleX(${menuCoords.width/100})  
       scaleY(${menuCoords.height/100})
     `
+    )
 
-    
-    
+    arrow.style.setProperty('transform',
+    `
+      translate(${menuCoords.left + (menuCoords.width / 2) - 7}px,${menuCoords.top - navCoords.top}px)
+      rotate(45deg) translateY(-50%)
+    `
     )
 }
 
