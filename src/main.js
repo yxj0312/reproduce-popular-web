38df5bbd30a808f7708756890da66acd0ff69dcb
yxj0312 <yanxiajun@hotmail.com>
2021-09-11 23:50:00 +0200

    change tailwind config

--- src/main.js
+++ src/main.js
@@ -12,8 +12,9 @@
 // `
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import './assets/styles/index.css'
 createApp(App).mount('#app')
+
 
 // const menuItems = document.querySelectorAll('nav ul li')
 // const menuBackground = document.querySelector('.menu-background')
