c41f7a2c9cab0bd503c076ae62e51008db8d7707
yxj0312 <yanxiajun@hotmail.com>
2021-09-12 21:39:07 +0200

    update collection images

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/styles/index.css'
+
 createApp(App).use(router).mount('#app')
 
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,12 @@
     <div class="bg-gray-100">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div class="max-w-2xl mx-auto py-16 sm:py-24 lg:py-32 lg:max-w-none">
-            <h2 class="text-2xl font-extrabold text-gray-900">Collections</h2>
+            <h2 class="text-2xl font-extrabold text-gray-900">Reproduce Popular Web Components</h2>
 
             <div class="mt-6 space-y-12 lg:space-y-0 lg:grid lg:grid-cols-3 lg:gap-x-6">
                 <div class="group relative">
                 <div class="relative w-full h-80 bg-white rounded-lg overflow-hidden group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1">
-                    <img src="https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg" alt="Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug." class="w-full h-full object-center object-cover">
+                    <img src="../assets/images/8fe7cbb2ec15c69f0d400d1aaaaf03b8.png" alt="Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug." class="w-full h-full object-center object-cover">
                 </div>
                 <h3 class="mt-6 text-sm text-gray-500">
                     <a href="#">
@@ -48,3 +48,4 @@
         </div>
     </div>
 </template>
+
