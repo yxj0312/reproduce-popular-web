6ac6523c48f0317cb7836477eada6800524bd7c7
yxj0312 <yanxiajun@hotmail.com>
2021-09-13 22:07:19 +0200

    add js

--- src/views/LcMixPanelScrollPin.vue
+++ src/views/LcMixPanelScrollPin.vue
@@ -257,3 +257,80 @@ img {
   visibility: hidden;
 }
 </style>
+
+<script>
+export default {
+  mounted() {
+    const sectionOneImage = document.querySelector('.image-one')
+const sectionTwoImage = document.querySelector('.image-two')
+const sectionThreeImage = document.querySelector('.image-three')
+
+const topValue = '252px'
+
+const options = {
+  threshold: 1,
+}
+
+const observerOne = new IntersectionObserver(entries => {
+  if (entries[0].boundingClientRect.y < 0) {
+    sectionOneImage.classList.add('fixed')
+    sectionOneImage.style.setProperty('top', topValue)
+
+    sectionTwoImage.classList.add('invisible')
+    sectionThreeImage.classList.add('invisible')
+  } else {
+    sectionOneImage.classList.remove('fixed')
+    sectionOneImage.classList.remove('invisible')
+
+    sectionTwoImage.classList.add('invisible')
+    sectionThreeImage.classList.add('invisible')
+  }
+}, options)
+
+observerOne.observe(document.querySelector('#pixel-to-watch-one'))
+
+
+const observerTwo = new IntersectionObserver(entries => {
+  console.log(entries[0])
+  if (entries[0].boundingClientRect.y < 0) {
+    sectionTwoImage.classList.remove('invisible')
+    sectionTwoImage.classList.add('fixed')
+    sectionTwoImage.style.setProperty('top', topValue)
+
+    sectionOneImage.classList.add('invisible')
+    sectionThreeImage.classList.add('invisible')
+  } else {
+    if (entries[0].isIntersecting && sectionOneImage.classList.contains('invisible')) {
+      sectionTwoImage.classList.remove('invisible')
+      sectionTwoImage.classList.add('fixed')
+      sectionTwoImage.style.setProperty('top', topValue)
+
+      sectionThreeImage.classList.add('invisible')
+    }
+  }
+}, options)
+
+observerTwo.observe(document.querySelector('#pixel-to-watch-two'))
+
+
+const observerThree = new IntersectionObserver(entries => {
+  if (entries[0].boundingClientRect.y < 0) {
+    sectionThreeImage.classList.remove('fixed')
+    sectionThreeImage.classList.remove('invisible')
+
+    sectionOneImage.classList.remove('fixed')
+    sectionTwoImage.classList.remove('fixed')
+
+    sectionOneImage.classList.add('invisible')
+    sectionTwoImage.classList.add('invisible')
+
+  } else {
+    sectionThreeImage.classList.add('fixed')
+    sectionThreeImage.style.setProperty('top', topValue)
+  }
+}, options)
+
+observerThree.observe(document.querySelector('#pixel-to-watch-three'))
+  },
+}
+</script>
