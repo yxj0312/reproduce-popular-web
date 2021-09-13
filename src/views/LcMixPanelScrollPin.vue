7c97e329e307e6c2f44672a4e74acc4884628f7f
yxj0312 <yanxiajun@hotmail.com>
2021-09-13 22:20:45 +0200

    update css

--- src/views/LcMixPanelScrollPin.vue
+++ src/views/LcMixPanelScrollPin.vue
@@ -232,7 +232,7 @@ img {
   height: 400px;
 }
 
-/* .section-one {
+.section-one {
   background: lightblue;
 }
 .section-two {
@@ -240,7 +240,7 @@ img {
 }
 .section-three {
   background: pink;
-} */
+}
 
 .mt-8 {
   margin-top: 240px;
@@ -262,75 +262,75 @@ img {
 export default {
   mounted() {
     const sectionOneImage = document.querySelector('.image-one')
-const sectionTwoImage = document.querySelector('.image-two')
-const sectionThreeImage = document.querySelector('.image-three')
+    const sectionTwoImage = document.querySelector('.image-two')
+    const sectionThreeImage = document.querySelector('.image-three')
 
-const topValue = '252px'
+    const topValue = '252px'
 
-const options = {
-  threshold: 1,
-}
-
-const observerOne = new IntersectionObserver(entries => {
-  if (entries[0].boundingClientRect.y < 0) {
-    sectionOneImage.classList.add('fixed')
-    sectionOneImage.style.setProperty('top', topValue)
-
-    sectionTwoImage.classList.add('invisible')
-    sectionThreeImage.classList.add('invisible')
-  } else {
-    sectionOneImage.classList.remove('fixed')
-    sectionOneImage.classList.remove('invisible')
-
-    sectionTwoImage.classList.add('invisible')
-    sectionThreeImage.classList.add('invisible')
-  }
-}, options)
-
-observerOne.observe(document.querySelector('#pixel-to-watch-one'))
-
-
-const observerTwo = new IntersectionObserver(entries => {
-  console.log(entries[0])
-  if (entries[0].boundingClientRect.y < 0) {
-    sectionTwoImage.classList.remove('invisible')
-    sectionTwoImage.classList.add('fixed')
-    sectionTwoImage.style.setProperty('top', topValue)
-
-    sectionOneImage.classList.add('invisible')
-    sectionThreeImage.classList.add('invisible')
-  } else {
-    if (entries[0].isIntersecting && sectionOneImage.classList.contains('invisible')) {
-      sectionTwoImage.classList.remove('invisible')
-      sectionTwoImage.classList.add('fixed')
-      sectionTwoImage.style.setProperty('top', topValue)
-
-      sectionThreeImage.classList.add('invisible')
+    const options = { 
+      threshold: 1,
     }
-  }
-}, options)
 
-observerTwo.observe(document.querySelector('#pixel-to-watch-two'))
+    const observerOne = new IntersectionObserver(entries => {
+      if (entries[0].boundingClientRect.y < 0) {
+        sectionOneImage.classList.add('fixed')
+        sectionOneImage.style.setProperty('top', topValue)
+
+        sectionTwoImage.classList.add('invisible')
+        sectionThreeImage.classList.add('invisible')
+      } else {
+        sectionOneImage.classList.remove('fixed')
+        sectionOneImage.classList.remove('invisible')
+
+        sectionTwoImage.classList.add('invisible')
+        sectionThreeImage.classList.add('invisible')
+      }
+    }, options)
+
+    observerOne.observe(document.querySelector('#pixel-to-watch-one'))
 
 
-const observerThree = new IntersectionObserver(entries => {
-  if (entries[0].boundingClientRect.y < 0) {
-    sectionThreeImage.classList.remove('fixed')
-    sectionThreeImage.classList.remove('invisible')
+    const observerTwo = new IntersectionObserver(entries => {
+      console.log(entries[0])
+      if (entries[0].boundingClientRect.y < 0) {
+        sectionTwoImage.classList.remove('invisible')
+        sectionTwoImage.classList.add('fixed')
+        sectionTwoImage.style.setProperty('top', topValue)
 
-    sectionOneImage.classList.remove('fixed')
-    sectionTwoImage.classList.remove('fixed')
+        sectionOneImage.classList.add('invisible')
+        sectionThreeImage.classList.add('invisible')
+      } else {
+        if (entries[0].isIntersecting && sectionOneImage.classList.contains('invisible')) {
+          sectionTwoImage.classList.remove('invisible')
+          sectionTwoImage.classList.add('fixed')
+          sectionTwoImage.style.setProperty('top', topValue)
 
-    sectionOneImage.classList.add('invisible')
-    sectionTwoImage.classList.add('invisible')
+          sectionThreeImage.classList.add('invisible')
+        }
+      }
+    }, options)
 
-  } else {
-    sectionThreeImage.classList.add('fixed')
-    sectionThreeImage.style.setProperty('top', topValue)
-  }
-}, options)
+    observerTwo.observe(document.querySelector('#pixel-to-watch-two'))
 
-observerThree.observe(document.querySelector('#pixel-to-watch-three'))
-  },
+
+    const observerThree = new IntersectionObserver(entries => {
+      if (entries[0].boundingClientRect.y < 0) {
+        sectionThreeImage.classList.remove('fixed')
+        sectionThreeImage.classList.remove('invisible')
+
+        sectionOneImage.classList.remove('fixed')
+        sectionTwoImage.classList.remove('fixed')
+
+        sectionOneImage.classList.add('invisible')
+        sectionTwoImage.classList.add('invisible')
+
+      } else {
+        sectionThreeImage.classList.add('fixed')
+        sectionThreeImage.style.setProperty('top', topValue)
+      }
+    }, options)
+
+    observerThree.observe(document.querySelector('#pixel-to-watch-three'))
+      },
 }
 </script>
